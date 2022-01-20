247b76a37408fe792e7d97bcdf7f62fd07a278e5
Drlazinho <lazbonfim@hotmail.com>
2022-01-20 10:18:38 -0300

    Desenvolvimento dos Layouts de listagen e detalhes das partidas.

---
+++ app/src/main/java/com/lazaropimentel/simulator/DetailActivity.kt
@@ -0,0 +1,20 @@
+package com.lazaropimentel.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lazaropimentel.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
