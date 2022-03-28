aa3f52d85337cdccf8a58ea0f59b90975b262cd4
Drlazinho <lazbonfim@hotmail.com>
2022-03-28 16:47:40 -0300

    Finalização do App de Simulação de Partidas

---
+++ app/src/main/java/com/lazaropimentel/simulator/ui/MainActivity.java
@@ -0,0 +1,114 @@
+package com.lazaropimentel.simulator.ui;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.lazaropimentel.simulator.R;
+import com.lazaropimentel.simulator.data.MatchesApi;
+import com.lazaropimentel.simulator.databinding.ActivityMainBinding;
+import com.lazaropimentel.simulator.domain.Match;
+import com.lazaropimentel.simulator.ui.adapter.MatchesAdapter;
+
+import java.util.List;
+import java.util.Random;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MatchesApi matchesApi;
+    private MatchesAdapter matchesAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setupHttpClient();
+        setupMatchesLis();
+        setupMatchesRefresh();
+        setupFloatActionButton();
+    }
+
+    private void setupHttpClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://digitalinnovationone.github.io/matches-simulator-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        matchesApi = retrofit.create(MatchesApi.class);
+    }
+
+    private void setupMatchesLis() {
+        binding.rvMatches.setHasFixedSize(true);
+        binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        findMatchesFromApi();
+    }
+
+
+
+    private void setupMatchesRefresh() {
+        binding.srlMatches.setOnRefreshListener(this::findMatchesFromApi);
+    }
+
+    private void setupFloatActionButton() {
+        binding.fabSimulate.setOnClickListener(view -> {
+            view.animate().rotationBy(360).setDuration(500).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    Random random = new Random();
+                    for (int i = 0; i < matchesAdapter.getItemCount(); i++) {
+                        Match match = matchesAdapter.getMatches().get(i);
+                        match.getHomeTeam().setScore(random.nextInt(match.getHomeTeam().getStars() + 1));
+                        match.getAwayTeam().setScore(random.nextInt(match.getAwayTeam().getStars() + 1));
+                        matchesAdapter.notifyItemChanged(i);
+                    }
+                }
+            });
+        });
+    }
+
+    private void findMatchesFromApi() {
+        binding.srlMatches.setRefreshing(true);
+        matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
+            @Override
+            public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
+                if(response.isSuccessful()){
+                    List<Match> matches = response.body();
+                    matchesAdapter = new MatchesAdapter(matches);
+                    binding.rvMatches.setAdapter(matchesAdapter);
+                } else {
+                    showErrorMessage();
+                }
+                binding.srlMatches.setRefreshing(false);
+            }
+
+            @Override
+            public void onFailure(Call<List<Match>> call, Throwable t) {
+                showErrorMessage();
+            }
+        });
+    }
+
+
+    private void showErrorMessage() {
+        Snackbar.make(binding.fabSimulate, R.string.error_api, Snackbar.LENGTH_SHORT).show();
+    }
+
+
+}
